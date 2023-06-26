e58fdb75b2e4ef4b071d9bb94b501b67e0e5f40b
seongmookK <118344507+seongmookK@users.noreply.github.com>
2023-06-26 16:39:38 +0900

    프론트엔드에서 서버, DB로 제출
    js, db, 서버

---
+++ frontend/write.js
@@ -0,0 +1,17 @@
+const form = document.getElementById("write-form");
+
+const handleSubmitForm = async (event) => {
+  event.preventDefault();
+  try {
+    const res = await fetch("/items", {
+      method: "POST",
+      body: new FormData(form),
+    });
+    const data = await res.json();
+    if (data === "200") window.location.pathname = "/";
+  } catch (e) {
+    console.error(e);
+  }
+};
+
+form.addEventListener("submit", handleSubmitForm);
